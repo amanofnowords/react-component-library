8b88586bf7565b8e4a558cc2631fae709623da9d
Mark Stanley <mark-stanley@outlook.com>
2020-11-25 14:13:47 -0500

    imp jest
    implemented jest along with react testing library

---
+++ src/TextInput/TextInput.test.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { TextInput } from '.';
+
+test('Input validation renders error message.', () => {
+    // Arrange
+    const inputID = 'test';
+    const inputAttributes = { 'data-testid': inputID };
+    const errorMessage = 'There is an error';
+    const errorMessageAttributes = { 'data-testid': 'errorMsg' };
+
+    const { getByTestId, queryByTestId } = render(
+        <TextInput
+            inputID={inputID}
+            errorMessage={errorMessage}
+            inputAttributes={inputAttributes}
+            errorMessageAttributes={errorMessageAttributes}
+            validate={true}
+        />,
+    );
+    // Act - change input
+    fireEvent.change(getByTestId(inputID), { target: { value: 'Mark3' } });
+
+    // Assert - error to be render
+    const $error = queryByTestId('errorMsg');
+    expect($error).toBeInTheDocument();
+});
+
+test('Input validation not render error message.', () => {
+    // Arrange
+    const inputID = 'test';
+    const inputAttributes = { 'data-testid': inputID };
+    const errorMessage = 'There is an error';
+    const errorMessageAttributes = { 'data-testid': 'errorMsg' };
+
+    const { getByTestId, queryByTestId } = render(
+        <TextInput
+            inputID={inputID}
+            errorMessage={errorMessage}
+            inputAttributes={inputAttributes}
+            errorMessageAttributes={errorMessageAttributes}
+            validate={true}
+        />,
+    );
+    // Act - change input
+    fireEvent.change(getByTestId(inputID), { target: { value: 'Mark' } });
+
+    // Assert - error to not be render
+    const $error = queryByTestId('errorMsg');
+    expect($error).not.toBeInTheDocument();
+});
--- src/TextInput/index.tsx
+++ src/TextInput/index.tsx
@@ -1,88 +1,87 @@
-import * as React from "react";
-import { UserEvent } from "../resources/types.js"
-import { TextInputProps } from "./TextInput.types.js"
+import * as React from 'react';
+import { UserEvent } from '../resources/types.js';
+import { TextInputProps } from './TextInput.types.js';
 
 const regexOptions: any = {
-  personName: /^[a-zA-Z]+$/,
-  email: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
-  postalCode: /^[a-zA-Z0-9äöüÄÖÜ]*$/
-}
+    personName: /^[a-zA-Z]+$/,
+    email: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+    postalCode: /^[a-zA-Z0-9äöüÄÖÜ]*$/,
+};
 
 const TextInput: React.FC<TextInputProps> = ({
-  inputID,
-  value,
-  placeholder = 'Please Enter Value',
-  label = 'Text Input: ',
-  containerClassName = '',
-  validate = false,
-  regexType = 'personName',
-  customRegex,
-  errorMessage = 'Error: Please check value',
-  onChangeCallback = undefined,
-  inputAttributes,
-  labelAttributes,
-  errorMessageAttributes
+    inputID,
+    value,
+    placeholder = 'Please Enter Value',
+    label = 'Text Input: ',
+    containerClassName = '',
+    validate = false,
+    regexType = 'personName',
+    customRegex,
+    errorMessage = 'Error: Please check value',
+    onChangeCallback = undefined,
+    inputAttributes,
+    labelAttributes,
+    errorMessageAttributes,
 }) => {
-  const [inputValue, setInputValue] = React.useState<string | number | undefined>(value)
+    const [inputValue, setInputValue] = React.useState<string | number | undefined>(value || '');
 
-  /** Takes in a value an checks to make sure it passes */
-  const validateInput: Function = (passedValue: string | number): boolean => {
-    let regex = regexOptions[regexType];
-    customRegex ? (regex = customRegex) : regex;
-    let isValid: boolean = regex.test(passedValue)
-    return isValid
-  };
+    /** Takes in a value an checks to make sure it passes */
+    const validateInput: Function = (passedValue: string | number): boolean => {
+        let regex = regexOptions[regexType];
+        customRegex ? (regex = customRegex) : regex;
+        let isValid: boolean = regex.test(passedValue);
+        return isValid;
+    };
 
-  if (validate === true) {
-    var [errorExist, setError] = React.useState<boolean>(!validateInput(inputValue))
-  }
-  const onInputValueChange: Function = (e: UserEvent) => {
-    setInputValue(e.target.value);
-    let isValid: boolean = validateInput(e.target.value);
-
-    // Validation check 
     if (validate === true) {
-      setError(!isValid);
+        var [errorExist, setError] = React.useState<boolean>(!validateInput(inputValue));
     }
-    if (onChangeCallback !== undefined) {
-      type Params = {
-        event: object
-        value: string | number
-        validate: boolean | undefined
-      }
-      let params: Params = {
-        event: e,
-        value: e.target.value,
-        validate: validate ? isValid : undefined,
-      }
-      onChangeCallback(params)
-    }
-  };
+    const onInputValueChange: Function = (e: UserEvent) => {
+        setInputValue(e.target.value);
+        let isValid: boolean = validateInput(e.target.value);
 
-  return (
-    <div className={`text-input ${containerClassName}`}>
-      <label htmlFor={inputID} {...labelAttributes}>
-        {label}
-      </label>
-      <input
-        type='text'
-        id={inputID}
-        placeholder={placeholder}
-        value={inputValue}
-        {...inputAttributes}
-        onChange={e => {
-          onInputValueChange(e)
-        }}
-      />
+        // Validation check
+        if (validate === true) {
+            setError(!isValid);
+        }
+        if (onChangeCallback !== undefined) {
+            type Params = {
+                event: object;
+                value: string | number;
+                validate: boolean | undefined;
+            };
+            let params: Params = {
+                event: e,
+                value: e.target.value,
+                validate: validate ? isValid : undefined,
+            };
+            onChangeCallback(params);
+        }
+    };
 
-      {errorExist === true && (
-        <p className='errorMessage' {...errorMessageAttributes}>
-          {errorMessage}
-        </p>
-      )}
-    </div>
-  )
+    return (
+        <div className={`text-input ${containerClassName}`}>
+            <label htmlFor={inputID} {...labelAttributes}>
+                {label}
+            </label>
+            <input
+                type="text"
+                id={inputID}
+                placeholder={placeholder}
+                value={inputValue}
+                {...inputAttributes}
+                onChange={(e) => {
+                    onInputValueChange(e);
+                }}
+            />
 
-}
+            {errorExist === true && (
+                <p className="errorMessage" {...errorMessageAttributes}>
+                    {errorMessage}
+                </p>
+            )}
+        </div>
+    );
+};
 
-export { TextInput }
+export { TextInput };
